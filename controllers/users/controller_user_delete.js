7dd517a25ef40d8b797ed23d0e0242066fe5791b
rseveric <rseveric@pros.com>
2019-11-28 10:20:08 -0800

    issue/001: Refactor Delete user

--- controllers/users/controller_user_delete.js
+++ controllers/users/controller_user_delete.js
@@ -5,8 +5,7 @@ const User =require('../../models/user')
 function deleteUser(request, response) {
 	let userId = request.params.userId
 	let dataBaseError = [];
-
-	User.findById(userId, (error, userStored) => {
+	User.findByIdAndRemove(userId, (error, userStored) => {
 		if(error){
 			dataBaseError.push("Data base Error");
 			dataBaseError.push(error);
@@ -14,16 +13,8 @@ function deleteUser(request, response) {
 		}
 		if(!userStored) {
 			return response.status(404).send({message: 'User does not exist'})
-		} else {
-			User.findByIdAndRemove(userId, (error, userStored) => {
-				if(error){
-					dataBaseError.push("Data base Error");
-					dataBaseError.push(error);
-					return response.status(500).send({dataBaseError})
-				}
-				response.status(200).send({message: 'User was deleted successfully'})
-			})
 		}
+		response.status(200).send({message: 'User was deleted successfully'})
 	})
 }
 

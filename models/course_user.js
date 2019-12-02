fbab8ad1a54b85db3c2ad125414546f8960a6180
rseveric <rseveric@pros.com>
2019-12-02 10:58:34 -0800

    issue/001: add ObjectId to collections

--- models/course_user.js
+++ models/course_user.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const CourseUserSchema = Schema({
-	_id: String,
-    id_couser: String,
-    id_user: String
+    id_couser: { type: ObjectId, ref: 'course' },
+    id_user: { type: String, ref: 'user' },
 })
 
 module.exports = mongoose.model('CourseUser', CourseUserSchema, 'course_user')
